5ab135f24033169d0a88846d6f4b9da79759172d
arwa4lc <arwaabdelrahem22@gmail.com>
2023-02-24 20:21:48 +0200

    feat: :sparkles: graceful shutdown example

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+mongoose.connect("mongodb://localhost:27015/test", (err) => {
+  if (err) throw err;
+  console.log("Mongoose connected!");
+});
+const User = mongoose.model("User", { name: String });
+
+app.post("/user", async (req, res) => {
+  try {
+    const user = new User({ name: req.body.username });
+    await user.save();
+    res.send("Success!").status(201);
+  } catch (err) {
+    res.send(err.message).status(500);
+  }
+});
+
+const server = app.listen(8080, () =>
+  console.log("Example app listening on port 8080!")
+);
+
+process.on("SIGTERM", () => {
+  console.info("SIGTERM signal received.");
+  console.log("Closing http server.");
+  server.close(() => {
+    console.log("Http server closed.");
+    // boolean means [force], see in mongoose doc
+    mongoose.connection.close(false, () => {
+      console.log("MongoDb connection closed.");
+      process.exit(0);
+    });
+  });
+});
